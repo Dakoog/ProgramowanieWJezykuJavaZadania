70a425e4f54032d629f20e45353505b85a45f4b7
Dakoog <taktoja@spoko.pl>
2022-04-26 13:10:53 +0200

    Added new package of conditional instructions with task1 and ManOfWoman class that gives gender for inserted people namee

---
+++ src/main/java/conditional_statements/task1/ManOfWoman.java
@@ -0,0 +1,28 @@
+package conditional_statements.task1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ManOfWoman {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(input);
+
+        System.out.println("Please insert name: ");
+        String name = br.readLine();
+        input.close();
+
+        StringBuilder otherName = new StringBuilder(name);
+        char lastLetter = otherName.charAt(otherName.length() - 1);
+        if (lastLetter == 'a' || lastLetter == 'e') {
+            if (!name.equals("Kuba") && !name.equals("Barnaba")) {
+                System.out.println(name +" is a woman.");
+            } else {
+                System.out.println(name +" is a man.");
+            }
+        } else {
+            System.out.println(name +" is a man.");
+        }
+    }
+}
